ee4d2f6f7caf3e888e37f6b8977cf775a17a89e2
JBuenafe13 <100136358+JBuenafe13@users.noreply.github.com>
2022-02-21 20:43:21 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Homepage</title>
+  </head>
+  <body>
+  <h1>Welcome to My Website! I am Ja Roan Anthony G. Buenafe!</h1>
+  <ul>
+    <li><a href="index.php">HOME</a></li>
+    <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+  </ul>
+  </body>
+</html>
