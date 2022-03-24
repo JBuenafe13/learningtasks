767ca77283b1ba88b4e302c067e43161a3de4fe7
JBuenafe13 <100136358+JBuenafe13@users.noreply.github.com>
2022-03-24 20:00:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <li><a href="index.php">HOME</a></li>
     <li><a href="/LT2/Homepage.html">LEARNING TASK 2</a></li>
     <li><a href="/LT3/10Favorites.html">LEARNING TASK 3</a></li>
+    <li><a href="/CLE1MT/Home.html">Culminating Learning Evidence (Midterm)</a></li>
   </ul>
   </body>
 </html>
