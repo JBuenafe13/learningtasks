e56404d34daf28fecfbdda5e3c3ee6480a45a8d4
JBuenafe13 <100136358+JBuenafe13@users.noreply.github.com>
2022-05-21 22:45:10 +0800

    Add files via upload

---
+++ CLE1ET/main.php
@@ -0,0 +1,12 @@
+<div id="main-wrapper">
+    <div id="main-content">
+        <h1>Hi I'm Ja Roan <span>Buenafe</span></h1>
+        <p>This is my Portfolion website
+        <br> It shows all my Learning Task and Websites that I created.
+            <br> I'am a <span class="typing">Programmer</span>
+        </p>
+    </div>
+    <div id="image">
+        <img src="Roan.png" class="boy">
+    </div>
+</div>
